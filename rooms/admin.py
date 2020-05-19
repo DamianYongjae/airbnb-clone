9c4ece95bb19903c8d21f9f1fc3b5301d8d2bc71
DamianYongjae <klick214@gmail.com>
2020-05-19 12:52:54 -0400

    room admin panel update

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,54 @@ class ItemAdmin(admin.ModelAdmin):
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facility", "house_rule",),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+    list_filter = (
+        "instant_book",
+        "host__superhost",  # foreign key
+        "room_tpye",
+        "amenities",
+        "facility",
+        "house_rule",
+        "city",
+        "country",
+    )
+
+    search_fields = ("^city", "^host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facility",
+        "house_rule",
+    )
 
 
 @admin.register(models.Photo)
