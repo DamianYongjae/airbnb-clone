a8621c00cd763ac224384cc18960f7c66f521460
DamianYongjae <klick214@gmail.com>
2020-05-20 15:59:23 -0400

    extended template

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def all_rooms(request):
     hungry = True
-    return render(request, "all_rooms.html", context={"hungry": hungry})
+    all_rooms = models.Room.objects.all()
+
+    return render(
+        request, "rooms/home.html", context={"hungry": hungry, "rooms": all_rooms}
+    )
